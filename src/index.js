6971ceee18fd3cab525152c399c31750cba2aeea
David Alen <davidalen.dev@gmail.com>
2022-06-25 23:37:43 -0300

    add: calls cb if error happens

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function(cb) {
-  return cb();
+async function throwTimeout(fn, cb) {
+  const t = setTimeout(() => {
+    cb(new Error());
+  }, 1000);
+
+  const d = fn(); // heavy operation
+
+  if (d.finally) {
+    d.finally?.(() => clearTimeout(t));
+    await d;
+  } else {
+    clearTimeout(t);
+  }
+
+  return d;
 }
+
+module.exports = throwTimeout;
--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,30 @@
 const assert = require('node:assert');
-const throwTimeout = require('./');
+const throwTimeout = require('.');
 
-// 1. accept a function that returns data, a == b
-const fn1 = () => 123;
-const r = throwTimeout(fn1);
+(async () => {
+  // 1. accept a function that returns data
+  const fn1 = () => 123;
+  const r1 = await throwTimeout(fn1);
 
-assert.equal(r, 123);
+  assert.equal(r1, 123);
 
-// 2. throw an error if function doesn't return data
+  // 2. pass with async function
+  const fn2 = async () => new Promise((res) => {
+    setTimeout(() => {
+      res('always');
+    }, 1);
+  });
+
+  assert.equal(await throwTimeout(fn2), 'always');
+
+  // 3. throw an error if function doesn't return data
+  const fn3 = () => new Promise((res) => {
+    setTimeout(() => {
+      res('never');
+    }, 2_000);
+  });
+
+  await throwTimeout(fn3, () => {
+    console.log('mata o objeto po');
+  });
+})();
